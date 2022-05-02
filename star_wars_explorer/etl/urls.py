d57f6c69d3b4e774d36670899bc92b516ce4a76a
Rafał Peisert <rafalmp@users.noreply.github.com>
2022-05-02 22:06:58 +0200

    Add count functionality

--- star_wars_explorer/etl/urls.py
+++ star_wars_explorer/etl/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from star_wars_explorer.etl.views import (
     CollectionDetailView,
     CollectionListView,
+    count,
     fetch_sw_data,
 )
 
@@ -10,6 +11,7 @@ app_name = "etl"
 
 urlpatterns = [
     path("", CollectionListView.as_view(), name="home"),
+    path("<int:pk>/count/", count, name="count"),
     path("<int:pk>/", CollectionDetailView.as_view(), name="details"),
     path("fetch/", fetch_sw_data, name="fetch"),
 ]
--- star_wars_explorer/etl/views.py
+++ star_wars_explorer/etl/views.py
@@ -3,6 +3,7 @@ from typing import Any
 import petl
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import DetailView, ListView
 
@@ -34,3 +35,12 @@ class CollectionDetailView(DetailView):
 def fetch_sw_data(request: HttpRequest) -> HttpResponse:
     transform_and_load()
     return HttpResponseRedirect(reverse("etl:home"))
+
+
+def count(request: HttpRequest, pk: int) -> HttpResponse:
+    collection = get_object_or_404(Collection, pk=pk)
+    table = petl.fromcsv(collection.csv_file.path)
+    counts = petl.valuecounts(table, request.POST["col1"], request.POST["col2"])
+    csv_data = petl.head(petl.cutout(counts, "frequency"), settings.LINES_PER_LOAD)
+    context = {"collection": collection, "csv_data": csv_data}
+    return render(request, "pages/count.html", context=context)
